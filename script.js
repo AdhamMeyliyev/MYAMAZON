2857da1cbef128669d48af2af5dbe03f90b87837
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2023-11-30 15:28:37 +0500

    MYAMAZON

---
+++ script.js
@@ -0,0 +1,11 @@
+/* let elHamburgerBtn = document.querySelector(".hamburger");
+let elCloseBtn = document.querySelector(".close");
+let elList = document.querySelector(".header__list1");
+
+elHamburgerBtn.addEventListener("click", () =>{
+    elList.style.display ="block";
+})
+
+elCloseBtn.addEventListener("click", () =>{
+    elList.style.display ="none";
+}) */
